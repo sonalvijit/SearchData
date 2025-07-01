8206a4b6fc11f6cdb7bcc22c11e7456e9f6fd6ed
svjt <josephtoppo.sxd@gmail.com>
2025-07-02 01:03:02 +0530

    Added a basic Search Bar

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import SearchBar from "./Components/SearchBar"
+import "./styles/core.css";
+
+function App() {
+
+  return (
+    <>
+      <SearchBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SearchBar.tsx
@@ -0,0 +1,18 @@
+const SearchBar = () => {
+     return (
+          <>
+               <div id="search-bar">
+                    <div id="search-bar-input">
+                         <input type="text" name="" id="" />
+                    </div>
+                    <div id="search-bar-button">
+                         <button type="submit">
+                              <img src="/search.svg" alt=""/>
+                         </button>
+                    </div>
+               </div>
+          </>
+     )
+};
+
+export default SearchBar;
